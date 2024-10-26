30382213c9e6f07e4d5be3930d27420b29f227cd
Nicola Getsee <Nicola.getsee@gmail.com>
2024-10-26 16:50:11 +0000

    commit with templates set up prior to database set up, django allauth set up

--- fitness/urls.py
+++ fitness/urls.py
@@ -12,9 +12,9 @@ from .views import (
 )
 
 urlpatterns = [
-    path('', home, name='home'),  # Home page as the root
+    path('', home, name='home'),  # Home page as root
     path('products/', products, name='products'),  # Products list page
-    path('product-list/', products, name='product-list'),  # Add this line for the product-list URL
+    path('product-list/', products, name='product-list'),  # product-list URL
     path('product/<int:product_id>/', product_detail, name='product_detail'),  # Product detail page
     path('subscription/', subscription, name='subscription'),  # Subscription page
     path('wishlist/', wishlist, name='wishlist'),  # Wishlist page
@@ -23,5 +23,5 @@ urlpatterns = [
      path('profile/', views.profile_view, name='profile'),  # Profile page view
     path('update-profile/', update_profile, name='update_profile'),  # User profile update page
     path('add_to_cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),  # Add to Cart view
-    # other product-related URLs...
+
 ]
