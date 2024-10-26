8f7a91ab1339fbe48b45222c514139c38d715c1c
Nicola Getsee <Nicola.getsee@gmail.com>
2024-10-26 17:22:21 +0000

    database setup

--- env.py
+++ env.py
@@ -1,6 +1,6 @@
 import os
 os.environ.setdefault(
-"DATABASE_URL", "postgres://u9qbmrznws6:tPoCTc6ovKLB@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/both_raid_daily_38548")
+"DATABASE_URL", "postgres://u9qbmrznws6:tPoCTc6ovKLB@ep-gentle-mountain-a23bxz6h-pooler.eu-central-1.aws.neon.tech/decal_bunch_drift_486028")
 os.environ.setdefault("SECRET_KEY", "<my_secret_key>")
 
 CLOUDINARY_STORAGE = {
