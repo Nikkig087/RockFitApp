f0fbba98d0eb9a46dac712ba146f7f916b8af394
Nicola Getsee <Nicola.getsee@gmail.com>
2024-12-17 18:32:38 +0000

    Removed non required index.html for GitHub Pages

--- RockFit/urls.py
+++ RockFit/urls.py
@@ -9,6 +9,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    
     path('admin/', admin.site.urls),
     path('', include('fitness.urls')),  
     path('cart/', include('cart.urls', namespace='cart')),  
