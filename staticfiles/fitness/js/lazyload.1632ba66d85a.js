0ee01b63dd9750af88cd2667ab00de8541b2bcb4
Nicola Getsee <Nicola.getsee@gmail.com>
2025-01-01 18:12:58 +0000

    added lazyload.js for image performance

---
+++ staticfiles/fitness/js/lazyload.1632ba66d85a.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const lazyImages = document.querySelectorAll("img[loading='lazy']");
+
+    const lazyLoad = (image) => {
+        image.src = image.dataset.src;
+        image.removeAttribute("data-src");
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                lazyLoad(entry.target);
+                observer.unobserve(entry.target);
+            }
+        });
+    });
+
+    lazyImages.forEach(image => observer.observe(image));
+});
